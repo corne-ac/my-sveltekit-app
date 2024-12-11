7adcc1a4c3fa6826f3d9e7529bd1106a16140f98
Corne <corne.ackerman1104@gmail.com>
2024-12-11 07:37:06 +0200

    table, code block, routes

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,7 @@
-<a>about</a>
+<div class="flex items-center justify-center">
+    <div class="mockup-code">
+        <pre data-prefix="$"><code>npm i /about</code></pre>
+        <pre data-prefix=">" class="text-warning"><code>installing...</code></pre>
+        <pre data-prefix=">" class="text-error"><code>Failed!</code></pre>
+      </div>
+</div>
